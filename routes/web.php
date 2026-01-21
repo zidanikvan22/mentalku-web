67d78c2b7b6f19774050ed58d9fdfd63d9fd0eff
zidanikvan22 <zidanikvan@gmail.com>
2026-01-21 21:23:08 +0700

    package, resources, routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', function () {
+    return view('app');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/dashboard', function () {
+    return view('user/dashboard');
+});
