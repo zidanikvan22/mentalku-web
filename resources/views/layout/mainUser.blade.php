0580cb3c18d7e3631aba653ef4d5c87b069fc2b6
zidanikvan22 <zidanikvan@gmail.com>
2026-01-21 21:20:08 +0700

    create layout mainUser

---
+++ resources/views/layout/mainUser.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="id" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'MentalKU')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            /* font-family: 'Tienne', serif;
+            font-weight: 400; */
+        }
+    </style>
+</head>
+
+<body class="bg-white text-gray-800 antialiased">
+
+    <div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+
+        <div class="drawer-content flex flex-col">
+
+            @include('component.navbar')
+
+            <main>
+                @yield('content')
+            </main>
+
+            {{-- @include('component.footer') --}}
+        </div>
+
+        <div class="drawer-side z-50">
+            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+
+            @include('component.sidebar')
+        </div>
+    </div>
+
+</body>
+
+</html>
