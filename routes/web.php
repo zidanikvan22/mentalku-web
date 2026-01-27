0553bc2de6011bd918e804f9aab98365e08ed25b
zidanikvan22 <zidanikvan@gmail.com>
2026-01-28 00:06:15 +0700

    update routes web.php

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,37 @@ Route::get('/test', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('user/dashboard');
+    return view('user.dashboard');
 });
 
 Route::get('/self-evaluation-cover', function () {
-    return view('user/self-evaluation-cover');
+    return view('user.self-evaluation-cover');
 });
 
 Route::get('/self-evaluation-question', function () {
-    return view('user/self-evaluation-question');
+    return view('user.self-evaluation-question');
 });
 
 Route::get('/self-evaluation-vent', function () {
-    return view('user/self-evaluation-vent');
+    return view('user.self-evaluation-vent');
+});
+
+Route::get('/self-evaluation-results', function () {
+    return view('user.self-evaluation-results');
+});
+
+Route::get('/education', function () {
+    return view('user.education');
+});
+
+Route::get('/education-detail', function () {
+    return view('user.education-detail');
+});
+
+Route::get('/profile', function () {
+    return view('user.profile');
+});
+
+Route::get('/profile-edit', function () {
+    return view('user.profile-edit');
 });
